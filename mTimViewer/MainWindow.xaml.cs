a8bda529dcc253ab1a9626be0058f94f605ec3b0
lrieger <lr@live.de>
2015-09-16 17:17:04 +0200

    add

---
+++ mTimViewer/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using HelixToolkit.Wpf;
+using Microsoft.Win32;
+using mTIM.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mTimViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.FileOk += (_, __) =>
+            {
+                var file = ofd.FileName;
+                Result r;
+                using (var fs = File.OpenRead(file))
+                    r = ProtoBuf.Serializer.Deserialize<Result>(fs);
+
+                _model.Children.Clear();
+
+                var material = MaterialHelper.CreateMaterial(Colors.Red, 1);
+
+                foreach (var ele in r.Elements)
+                {
+                    foreach (var geoRef in ele.Geometries)
+                    {
+                        var geo = r.Geometries[geoRef.GeometryIndex];
+                        var triangulated = geo as TriangulatedGeometryData;
+                        if (triangulated != null)
+                        {
+                            var builder = new MeshBuilder(true, false);
+                            foreach (var t in triangulated.Triangles)
+                            {
+                                var a = r.PointsAndVectors[t.A].ToWpfPoint();
+                                var b = r.PointsAndVectors[t.B].ToWpfPoint();
+                                var c = r.PointsAndVectors[t.C].ToWpfPoint();
+                                builder.AddTriangle(a, c, b);
+                            }
+                            var mesh = builder.ToMesh(true);
+                            var geoModel = new GeometryModel3D { Geometry = mesh, Material = material };
+                            
+                            if (geoRef.Transform != null)
+                                geoModel.Transform = new MatrixTransform3D(MatrixHelper.FromArray(geoRef.Transform.Matrix));
+                            var modelVisual = new ModelVisual3D { Content = geoModel };
+
+                            //foreach (var l in triangulated.Lines)
+                            //{
+                            //    var a = r.PointsAndVectors[l.A].ToWpfPoint();
+                            //    var b = r.PointsAndVectors[l.B].ToWpfPoint();
+                            //    var lineGeo = new LinesVisual3D { Color = Colors.Green, Points = new[] { a, b } };
+                            //    if (matrix != null)
+                            //        lineGeo.Transform = new MatrixTransform3D(matrix.Value);
+
+                            //    modelVisual.Children.Add(lineGeo);
+                            //}
+
+                            _model.Children.Add(modelVisual);
+                        }
+                        else
+                        {
+                            throw new Exception("unknown geo data: " + geo.GetType());
+                        }
+                    }
+                }
+                _viewPort.ZoomExtents(500);
+            };
+            ofd.ShowDialog();
+        }
+    }
+
+    public static class PointHelper
+    {
+        public static Point3D ToWpfPoint(this XYZ self)
+        {
+            return new Point3D { X = self.X, Y = self.Y, Z = self.Z };
+        }
+    }
+
+    public static class MatrixHelper
+    {
+        public static Matrix3D FromArray(float[] matrix)
+        {
+            // TIM saves the Matrix in Column-Major
+            // WPF (DirectX) uses Row-Major
+            return new Matrix3D(matrix[0], matrix[4], matrix[8], matrix[12], matrix[1], matrix[5], matrix[9], matrix[13], matrix[2], matrix[6], matrix[10], matrix[14], matrix[3], matrix[7], matrix[11], matrix[15]);
+        }
+    }
+}
---
+++ mTimViewer/Schema.cs
@@ -0,0 +1,385 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mTIM.Graphics
+{
+    [ProtoContract]
+    [ProtoInclude(1, typeof(TriangulatedGeometryData))]
+    public abstract class GeometryData : IEquatable<GeometryData>, IComparable<GeometryData>
+    {
+        public abstract bool Equals(GeometryData other);
+        public abstract int CompareTo(GeometryData other);
+    }
+
+    [ProtoContract]
+    public class Transform : IEquatable<Transform>, IComparable<Transform>
+    {
+        [ProtoMember(1, IsRequired = true)]
+        public float[] Matrix { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var otherTransform = obj as Transform;
+            if (false == Object.ReferenceEquals(otherTransform, null))
+                return this.Equals(otherTransform);
+            else
+                return false;
+        }
+
+        public bool Equals(Transform other)
+        {
+            if (Object.ReferenceEquals(other, null)) return false;
+            return this.Matrix.SequenceEqual(other.Matrix);
+        }
+
+        public int CompareTo(Transform other)
+        {
+            if (Object.ReferenceEquals(other, null)) return 1;
+            for (int i = 0; i < this.Matrix.Length; i++)
+            {
+                var c = this.Matrix[i].CompareTo(other.Matrix[i]);
+                if (c != 0)
+                    return c;
+            }
+            return 0;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            foreach (var f in Matrix)
+                unchecked { hash = hash * 486187739 + f.GetHashCode(); }
+            return hash;
+        }
+    }
+
+    [ProtoContract]
+    public struct XYZ : IEquatable<XYZ>, IComparable<XYZ>
+    {
+        [ProtoMember(1, IsRequired = true)]
+        public float X { get; set; }
+        [ProtoMember(2, IsRequired = true)]
+        public float Y { get; set; }
+        [ProtoMember(3, IsRequired = true)]
+        public float Z { get; set; }
+
+        bool IEquatable<XYZ>.Equals(XYZ other)
+        {
+            return (this.X == other.X) &&
+                    (this.Y == other.Y) &&
+                    (this.Z == other.Z);
+        }
+
+        int IComparable<XYZ>.CompareTo(XYZ other)
+        {
+            var cx = this.X.CompareTo(other.X);
+            if (cx == 0)
+            {
+                var cy = this.Y.CompareTo(other.Y);
+                if (cy == 0)
+                {
+                    return this.Z.CompareTo(other.Z);
+                }
+                else return cy;
+            }
+            else return cx;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            unchecked
+            {
+                hash = hash * 486187739 + X.GetHashCode();
+                hash = hash * 486187739 + Y.GetHashCode();
+                hash = hash * 486187739 + Z.GetHashCode();
+            }
+            return hash;
+        }
+    }
+
+    [ProtoContract]
+    public class Triangle : IEquatable<Triangle>, IComparable<Triangle>
+    {
+        [ProtoMember(1, IsRequired = true)]
+        public uint A { get; set; }
+        [ProtoMember(2, IsRequired = true)]
+        public uint B { get; set; }
+        [ProtoMember(3, IsRequired = true)]
+        public uint C { get; set; }
+
+        bool IEquatable<Triangle>.Equals(Triangle other)
+        {
+            if (Object.ReferenceEquals(other, null)) return false;
+            return this.A == other.A &&
+                    this.B == other.B &&
+                    this.C == other.C;
+        }
+
+        int IComparable<Triangle>.CompareTo(Triangle other)
+        {
+            if (Object.ReferenceEquals(other, null)) return 1;
+            var ca = this.A.CompareTo(other.A);
+            if (ca == 0)
+            {
+                var cb = this.B.CompareTo(other.B);
+                if (cb == 0)
+                {
+                    var cc = this.C.CompareTo(other.C);
+                    return cc;
+                }
+                else return cb;
+            }
+            else return ca;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash = hash * 486187739 + A.GetHashCode();
+            hash = hash * 486187739 + B.GetHashCode();
+            hash = hash * 486187739 + C.GetHashCode();
+            return hash;
+        }
+    }
+
+    [ProtoContract]
+    public class Line : IEquatable<Line>, IComparable<Line>
+    {
+        [ProtoMember(1, IsRequired = true)]
+        public uint A { get; set; }
+        [ProtoMember(2, IsRequired = true)]
+        public uint B { get; set; }
+
+        bool IEquatable<Line>.Equals(Line other)
+        {
+            if (Object.ReferenceEquals(other, null)) return false;
+            return this.A == other.A &&
+                    this.B == other.B;
+        }
+
+        int IComparable<Line>.CompareTo(Line other)
+        {
+            if (Object.ReferenceEquals(other, null)) return 1;
+            var ca = this.A.CompareTo(other.A);
+            if (ca == 0)
+            {
+                var cb = this.B.CompareTo(other.B);
+                return cb;
+            }
+            else return ca;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash = hash * 486187739 + A.GetHashCode();
+            hash = hash * 486187739 + B.GetHashCode();
+            return hash;
+        }
+    }
+
+    [ProtoContract]
+    public class TriangulatedGeometryData : GeometryData, IEquatable<TriangulatedGeometryData>, IComparable<TriangulatedGeometryData>
+    {
+        [ProtoMember(1, IsRequired = true)]
+        public Triangle[] Triangles { get; set; }
+        [ProtoMember(2, IsRequired = false)]
+        public Line[] Lines { get; set; }
+
+        public bool Equals(TriangulatedGeometryData other)
+        {
+            if (this.Triangles.Length != other.Triangles.Length)
+                return false;
+
+            var thisLinesNull = Object.ReferenceEquals(null, this.Lines);
+            var otherLinesNull = Object.ReferenceEquals(null, other.Lines);
+            if (thisLinesNull != otherLinesNull)
+                return false;
+            if (false == thisLinesNull)
+            {
+                if (this.Lines.Length != other.Lines.Length)
+                    return false;
+                if (false == this.Lines.SequenceEqual(other.Lines))
+                    return false;
+            }
+            return this.Triangles.SequenceEqual(other.Triangles);
+        }
+
+        public int CompareTo(TriangulatedGeometryData other)
+        {
+            if (Object.ReferenceEquals(other, null)) return 1;
+
+            // compare length
+            if (this.Triangles.Length != other.Triangles.Length)
+                return this.Triangles.Length.CompareTo(other.Triangles.Length);
+
+            // length equal, compare elements
+            for (int i = 0; i < Triangles.Length; i++)
+            {
+                var c = ((IComparable<Triangle>)this.Triangles[i]).CompareTo(other.Triangles[i]);
+                if (c != 0)
+                    return c;
+            }
+
+            // elements equal
+            return 0;
+        }
+
+        public override bool Equals(GeometryData other)
+        {
+            var otherTriangulated = other as TriangulatedGeometryData;
+            if (otherTriangulated != null)
+            {
+                return this.Equals(otherTriangulated);
+            }
+            else return false;
+        }
+
+        public override int CompareTo(GeometryData other)
+        {
+            var otherTriangulated = other as TriangulatedGeometryData;
+            if (otherTriangulated != null)
+            {
+                return this.CompareTo(otherTriangulated);
+            }
+            else return 1;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherTriangulated = obj as TriangulatedGeometryData;
+            if (otherTriangulated != null)
+            {
+                return ((IEquatable<TriangulatedGeometryData>)this).Equals(otherTriangulated);
+            }
+            else return false;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            foreach (var t in Triangles)
+                unchecked { hash = hash * 486187739 + t.GetHashCode(); }
+            return hash;
+        }
+    }
+
+    [ProtoContract]
+    public class GeometryReference
+    {
+        /// <summary>
+        /// Index into Result.Geometries
+        /// </summary>
+        [ProtoMember(1, IsRequired = true)]
+        public uint GeometryIndex { get; set; }
+        /// <summary>
+        /// A (optional) transform applied to the geometry.
+        /// </summary>
+        [ProtoMember(2)]
+        public Transform Transform { get; set; }
+        /// <summary>
+        /// Defines how the geometry should be painted.
+        /// </summary>
+        [ProtoMember(3, IsRequired = true)]
+        public string ColorTag { get; set; }
+        /// <summary>
+        /// In which Layer this Geometry is.
+        /// (Concrete, Reinforcement, ...)
+        /// </summary>
+        [ProtoMember(4, IsRequired = true)]
+        public string LayerTag { get; set; }
+    }
+
+    [ProtoContract]
+    public class Visualizable
+    {
+        /// <summary>
+        /// The ID of the Element, referenced from externally
+        /// </summary>
+        [ProtoMember(1, IsRequired = true)]
+        public Guid Id { get; set; }
+        /// <summary>
+        /// The Geometries which form this Visualizable.
+        /// </summary>
+        [ProtoMember(2, IsRequired = true)]
+        public GeometryReference[] Geometries { get; set; }
+    }
+
+    [ProtoContract]
+    public class Result
+    {
+        /// <summary>
+        /// All Precast Elements.
+        /// </summary>
+        [ProtoMember(1, IsRequired = true)]
+        public Visualizable[] Elements { get; set; }
+        /// <summary>
+        /// The geometries of the Elements.
+        /// </summary>
+        [ProtoMember(2, IsRequired = true)]
+        public GeometryData[] Geometries { get; set; }
+        /// <summary>
+        /// A Vertex List shared by all Geometries.
+        /// </summary>
+        [ProtoMember(3, IsRequired = true)]
+        public XYZ[] PointsAndVectors { get; set; }
+
+
+        public static string GetProtoDefinition()
+        {
+            return ProtoBuf.Serializer.GetProto<Result>();
+        }
+    }
+
+
+    //####################################
+    //## Just examples, not implemented ##
+    //####################################
+
+
+    public abstract class Node
+    {
+        /// <summary>
+        /// optional
+        /// </summary>
+        public Node Parent { get; set; }
+
+        /// <summary>
+        /// optional
+        /// </summary>
+        public IList<Node> Children { get; set; }
+    }
+
+    public class Leaf : Node
+    {
+        public IList<PrecastElementRef> Elements { get; set; }
+    }
+
+    public class PrecastElementRef
+    {
+        public Guid PrecastElementId { get; set; }
+        public Transform Transform { get; set; }
+    }
+
+    public class ReinforcementGeometryData : GeometryData
+    {
+        public XYZ[] PolyLine3D { get; set; }
+        public float Diameter { get; set; }
+        public float BendingRadius { get; set; }
+
+        public override bool Equals(GeometryData other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int CompareTo(GeometryData other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
